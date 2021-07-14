090461f148867970ba4daccf9897294e62cec4b9
髙嶌海登 <kaitotakashima@umitounoMacBook-Pro.local>
2021-07-14 17:10:06 +0900

    20210714

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const demo = (num) => {
+    console.log(num);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
